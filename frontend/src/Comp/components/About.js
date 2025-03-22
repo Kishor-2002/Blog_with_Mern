a4f3b7f9d5ee70ac7de8851d7ba54be922088977
Kishor-2002 <93820212+Kishor-2002@users.noreply.github.com>
2025-03-22 10:40:37 +0530

    Create About.js

---
+++ frontend/src/Comp/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <b>Full Stack Blog Application</b>
+        <p>Paragraph about this application</p>
+        <ul>
+            <li>short note on <b>React</b> and its uses in this application</li>
+            <li>short note on <b>Node</b> and its uses in this application</li>
+            <li>short note on <b>Express</b> and its uses in this application</li>
+            <li>short note on <b>MongoDB</b> and its uses in this application</li>
+            <li>short note on <b>JWT</b> and its uses in this application</li>
+            <li>short note on <b>Other Technologies</b> and its uses in this application</li>
+        </ul>
+    </div>
+  )
+}
+
+export default About
