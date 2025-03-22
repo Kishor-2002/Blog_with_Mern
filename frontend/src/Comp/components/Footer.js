08485b412465fe72d26d339c6c3a433b0bccf2bb
Kishor-2002 <93820212+Kishor-2002@users.noreply.github.com>
2025-03-22 10:46:46 +0530

    Create Footer.js

---
+++ frontend/src/Comp/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react' 
+
+const Footer = () =>{
+    return (
+        <footer>&copy; {new Date().getFullYear()} Built with NodeJs & MongoDB</footer>
+    )
+}
+export default Footer;
